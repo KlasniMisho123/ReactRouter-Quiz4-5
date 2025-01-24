f382b31094e605c83dee5ec7dbea1d5227cf265f
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-01-24 21:53:27 +0400

    hero page

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='hero-flex'>
+        <img className='hero-img' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9dCkCYFcXoIWkrUi0nMkjklfwjFRx4zP5Og&s" alt='monkey king'/>
+        <div className='hero-div' >
+            <h1>HeroTitle</h1>
+            <p>HeroText</p>
+        </div>
+    </div>
+  )
+}

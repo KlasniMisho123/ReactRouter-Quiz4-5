b27e2156564eca5172cfc950462a3be9142b6567
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-01-24 23:12:14 +0400

    hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import heroDatas from "../heroCalc";
 
 export default function Hero() {
-  return (
-    <div className='hero-flex'>
-        {/* <img className='hero-img' src="monkeyKing.jpg" alt='monkey king'/> */}
-        {/* <img className='hero-img' src="https://www.rockpapershotgun.com/images/16/aug/sun1.jpg" alt='monkey king'/> */}
-        <img className='hero-img' src="https://dmarket.com/blog/best-dota2-wallpapers/monkey-king-dota-2-wallpapers_hu12480426162898230189.jpg" alt='monkey king'/>
-        <div className='hero-div' >
-            <h1 className='hero-title' >GrimStroke</h1>
-            <p className='hero-desc'>Grimstroke was once a noble artist whose ambition led him to sacrifice his entire village to a cursed, 
-                unholy ink. In his lust for power, he bound their souls to the ink, dooming them and cursing himself in
-                 the process. Now, he wields the same ink as a weapon, painting grim fates for his enemies while haunted by his dark past.</p>
-            <h4 className='hero-v-line'>"Your stroke of fate ends here"</h4>
-            <button className='redirect-btn'> Go To Gallery </button>
-        </div>
-    </div>
-  )
+    const [currnetNum, setCurrnetNum] = useState(-1);
+    const [currentHero, setCurrentHero] = useState(null);
+
+    useEffect(() => {
+        // Generate a random number between 0 and heroDatas length
+        let randNum = Math.floor(Math.random() * heroDatas.length);
+        setCurrnetNum(randNum);
+    }, []);
+
+    useEffect(() => {
+        if (currnetNum !== -1) {
+            // setCurrentHero(heroDatas[currnetNum]);
+            setCurrentHero(heroDatas[1]);
+        }
+    }, [currnetNum]);
+
+    console.log(currentHero);
+
+    return (
+        <>
+            {currentHero ? (
+                <div className='hero-flex'>
+                    <img className='hero-img' src={currentHero.imgSrc} alt={currentHero.name} />
+                    <div className='hero-div'>
+                        <h1 className='hero-title'>{currentHero.name}</h1>
+                        <p className='hero-desc'>{currentHero.description}</p>
+                        <h4 className='hero-v-line'>"{currentHero.voiceLine}"</h4>
+                        <button className='redirect-btn'>Go To Gallery</button>
+                    </div>
+                </div>
+            ) : (
+                <div>Loading...</div>
+            )}
+        </>
+    );
 }
---
+++ src/heroCalc.js
@@ -0,0 +1,73 @@
+const heroes = [
+    {
+        name: "Grimstroke",
+        imgSrc:"https://dmarket.com/blog/best-dota2-wallpapers/monkey-king-dota-2-wallpapers_hu12480426162898230189.jpg",
+        description: `Grimstroke was once a noble artist whose ambition led him to sacrifice his entire village to a cursed, 
+                unholy ink. In his lust for power, he bound their souls to the ink, dooming them and cursing himself in
+                 the process. Now, he wields the same ink as a weapon, painting grim fates for his enemies while haunted by his dark past.`,
+        voiceLine: "Your stroke of fate ends here"
+    },
+    {
+        name: "Invoker",
+        imgSrc:"https://external-preview.redd.it/fqIurKBOSIhruhJxKlbBrrM9eRJEB5FBN5Tff4kDf1E.jpg?auto=webp&s=c4bbe83af0aebfa69fd7bf4948a50badbc7674b9",
+        description: `Invoker is a master of arcane magic, drawing on the power of the elements to cast powerful and devastating spells. 
+                      With his vast knowledge of sorcery, he can manipulate the forces of nature and adapt his abilities to any situation.`,
+        voiceLine: "I am the master of the arcane!"
+    },{
+        name: "Pudge",
+        imgSrc:"https://dmarket.com/blog/best-dota2-wallpapers/monkey-king-dota-2-wallpapers_hu12480426162898230189.jpg",
+        // imgSrc: "https://dmarket.com/blog/best-dota2-wallpapers/pudge-dota-2-wallpapers_hu12480426162898230192.jpg",
+        description: `Pudge is a grotesque and terrifying brute, known for his ability to hook enemies and drag them 
+                      into his grasp, where he can dismember them with his meat cleaver and feast on their remains.`,
+        voiceLine: "Mmmm... fresh meat!"
+    },
+    {
+        name: "Crystal Maiden",
+        // imgSrc: "https://dmarket.com/blog/best-dota2-wallpapers/crystal-maiden-dota-2-wallpapers_hu12480426162898230193.jpg",
+        imgSrc:"https://dmarket.com/blog/best-dota2-wallpapers/monkey-king-dota-2-wallpapers_hu12480426162898230189.jpg",
+        description: `Crystal Maiden is a mage of the winter's chill, capable of controlling the frost and ice around her 
+                      to freeze her enemies in place and call down devastating winter storms.`,
+        voiceLine: "Let it snow!"
+    },
+    {
+        name: "Phantom Assassin",
+        // imgSrc: "https://dmarket.com/blog/best-dota2-wallpapers/phantom-assassin-dota-2-wallpapers_hu12480426162898230194.jpg",
+        imgSrc:"https://dmarket.com/blog/best-dota2-wallpapers/monkey-king-dota-2-wallpapers_hu12480426162898230189.jpg",
+        description: `Phantom Assassin is a deadly and elusive assassin who strikes swiftly from the shadows, 
+                      delivering critical blows with unmatched precision.`,
+        voiceLine: "Death is at your door."
+    },
+    {
+        name: "Monkey King",
+        // imgSrc: "https://dmarket.com/blog/best-dota2-wallpapers/monkey-king-dota-2-wallpapers_hu12480426162898230195.jpg",
+        imgSrc:"https://dmarket.com/blog/best-dota2-wallpapers/monkey-king-dota-2-wallpapers_hu12480426162898230189.jpg",
+        description: `Monkey King is a master of martial arts and trickery, capable of summoning copies of himself 
+                      and unleashing devastating aerial attacks with his magical staff.`,
+        voiceLine: "I am the king of the jungle!"
+    },
+    {
+        name: "Legion Commander",
+        // imgSrc: "https://dmarket.com/blog/best-dota2-wallpapers/legion-commander-dota-2-wallpapers_hu12480426162898230196.jpg",
+        imgSrc:"https://dmarket.com/blog/best-dota2-wallpapers/monkey-king-dota-2-wallpapers_hu12480426162898230189.jpg",
+        description: `Legion Commander is a fearless and honorable warrior, known for her ability to challenge her enemies 
+                      to one-on-one combat. Her unmatched resolve allows her to grow stronger with each victory in battle.`,
+        voiceLine: "Prepare for battle!"
+    },{
+        name: "Lina",
+        // imgSrc: "https://dmarket.com/blog/best-dota2-wallpapers/lina-dota-2-wallpapers_hu12480426162898230197.jpg",
+        imgSrc:"https://dmarket.com/blog/best-dota2-wallpapers/monkey-king-dota-2-wallpapers_hu12480426162898230189.jpg",
+        description: `Lina is a fiery mage who wields the power of the sun, burning her enemies with her powerful flame spells. 
+                      Her speed and damage make her a fearsome adversary, capable of overwhelming opponents in seconds.`,
+        voiceLine: "I bring the heat!"
+    },
+    {
+        name: "Sven",
+        // imgSrc: "https://dmarket.com/blog/best-dota2-wallpapers/sven-dota-2-wallpapers_hu12480426162898230198.jpg",
+        imgSrc:"https://dmarket.com/blog/best-dota2-wallpapers/monkey-king-dota-2-wallpapers_hu12480426162898230189.jpg",
+        description: `Sven is a mighty warrior with the strength to crush his enemies with his enormous hammer. He is known for 
+                      his ability to deliver devastating blows in the heat of battle, and his determination makes him a great ally.`,
+        voiceLine: "None can stand against me!"
+    }
+];
+
+export default heroes;

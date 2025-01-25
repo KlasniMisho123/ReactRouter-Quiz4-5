d90188b0be1a565fa0c0aee394b4b43bd6e9a8ea
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-01-25 23:57:01 +0400

    Gallery commit

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+
+const api = axios.create({
+    baseURL:"https://jsonplaceholder.typicode.com/photos?_limit=4"
+})
 
 export default function Gallery() {
+const [apiResponseData, setApiResponseData] = useState([]);
+
+async function fetchData() {
+    try {
+      const response = await api.get("/");
+      setApiResponseData(response.data)
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+useEffect(()=> {
+    fetchData()
+},[]) 
+
+useEffect(() => {
+    console.log(apiResponseData); 
+  }, [apiResponseData]);
+
   return (
-    <div>Gallery</div>
+    <div>
+        <h1 className='gallery-title'>Gallery</h1>
+        <div className='album-flex'>
+        {(
+        apiResponseData.map((item) => (
+            <div key={item.id} className={`album-item `}>
+                <div className={`album-img-div ${item.id === 1 ? "yellow" : item.id === 2 ? "purple" : item.id === 3 ? "green" : item.id === 4 ? "pink" : ""}`} > 
+                    <img src={item.thumbnailUrl} alt={item.title} />
+                </div>
+                    <p className='album-title'>{item.title}</p>
+            </div>
+            ))
+        )}
+        </div>
+    </div>
   )
 }

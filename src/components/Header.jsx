904caf138ea7f80eef16e1d7960d81bc5e17d115
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-01-26 16:34:09 +0400

    header endpoint commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import DotaGalleryLogo from './DotaGalleryLogo'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 export default function Header() {
-  const [currentPage, setCurrentPage] = useState("")
+  let location = useLocation();
+  const [currentPage, setCurrentPage] = useState( location.pathname)
 
   function handleSelectedNav(selected) {
     setCurrentPage(selected)
   }
 
+  
+  
   return (
     <header>
       <div className='header-pos'> 
       <button onClick={()=>{handleSelectedNav("")}} className={"invisible-btn " + (currentPage === "" ? 'active-page-button' : "")}> <DotaGalleryLogo /> </ button>
         <nav className='nav-bar'>
           <button onClick={()=>{handleSelectedNav("")}} className={currentPage === "" ? 'active-page-button' : ""}> <Link to="/">Home</Link> </button>
-          <button onClick={()=>{handleSelectedNav("gallery")}} className={currentPage === "gallery" ? 'active-page-button' : ""}> <Link to="/gallery">Gallery</Link> </button>
-          <button onClick={()=>{handleSelectedNav("profile")}} className={currentPage === "profile" ? 'active-page-button' : ""}> <Link to="/profile">Profile</Link> </button>
+          <button onClick={()=>{handleSelectedNav("/gallery")}} className={currentPage === "/gallery" ? 'active-page-button' : ""}> <Link to="/gallery">Gallery</Link> </button>
+          <button onClick={()=>{handleSelectedNav("/profile")}} className={currentPage === "/profile" ? 'active-page-button' : ""}> <Link to="/profile">Profile</Link> </button>
         </nav>
       </div>
     </header>
